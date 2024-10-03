63fcba05fdf43d4d2fc23b1db2e3d1fd352fb1ed
Michael Umeokoli <michaelumeokoli@Michaels-MBP.lan>
2024-10-03 16:42:08 +0100

    cors 2

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ origins = [ "http://localhost:5173", "http://localhost:3000", "http://localhost:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173/","https://perspectify-rho.vercel.app", "http://perspectify-rho.vercel.app/", "https://perspectify-rho.vercel.app/"],  # Allow all origins
-    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["X-Requested-With", "Content-Type"],
 )
 
 # origins = ["*"]
